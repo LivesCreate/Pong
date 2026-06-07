cb71d750290dcf8a47d2f05d592a5eebf8196cbe
LivesCreate <252925942+LivesCreate@users.noreply.github.com>
2026-06-07 09:39:39 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Pong service worker
-const CACHE_NAME = "pong-v1.9.0";
+const CACHE_NAME = "pong-v1.10.0";
 const APP_SHELL = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", function (event) {
